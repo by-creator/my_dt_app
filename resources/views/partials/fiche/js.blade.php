7e90a20e36ae12823d8ce76890d1032ddd6013e6
by-creator <marcdamien04@gmail.com>
2025-10-29 16:53:24 +0000

    update

--- resources/views/partials/fiche/js.blade.php
+++ resources/views/partials/fiche/js.blade.php
@@ -1,14 +1,14 @@
  <!-- Vendor JS Files -->
- <script src="{{asset('fiche/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
- <script src="{{asset('fiche/assets/vendor/php-email-form/validate.js')}}"></script>
- <script src="{{asset('fiche/assets/vendor/aos/aos.js')}}"></script>
- <script src="{{asset('fiche/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
- <script src="{{asset('fiche/assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
- <script src="{{asset('fiche/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
- <script src="{{asset('fiche/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js')}}"></script>
- <script src="{{asset('fiche/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+ <script src="{{asset('templates/fiche/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+ <script src="{{asset('templates/fiche/assets/vendor/php-email-form/validate.js')}}"></script>
+ <script src="{{asset('templates/fiche/assets/vendor/aos/aos.js')}}"></script>
+ <script src="{{asset('templates/fiche/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+ <script src="{{asset('templates/fiche/assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
+ <script src="{{asset('templates/fiche/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+ <script src="{{asset('templates/fiche/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js')}}"></script>
+ <script src="{{asset('templates/fiche/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
 
  <!-- Main JS File -->
- <script src="{{asset('fiche/assets/js/main.js')}}"></script>
- <script src="{{asset('fiche/assets/js/estimate_conteneur.js')}}"></script>
- <script src="{{asset('fiche/assets/js/estimate_vehicule.js')}}"></script>
+ <script src="{{asset('templates/fiche/assets/js/main.js')}}"></script>
+ <script src="{{asset('templates/fiche/assets/js/estimate_conteneur.js')}}"></script>
+ <script src="{{asset('templates/fiche/assets/js/estimate_vehicule.js')}}"></script>
