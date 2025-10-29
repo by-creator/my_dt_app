853328ab4f98e081cdd20b7ffe1bfb753d2a6bc1
by-creator <marcdamien04@gmail.com>
2025-10-29 16:51:49 +0000

    update

--- resources/views/partials/fiche/head.blade.php
+++ resources/views/partials/fiche/head.blade.php
@@ -5,8 +5,8 @@
     <meta name="keywords" content="">
 
     <!-- Favicons -->
-    <link href="{{asset('fiche/assets/img/logo.png')}}" rel="icon">
-    <link href="{{asset('fiche/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+    <link href="{{asset('templates/fiche/assets/img/logo.png')}}" rel="icon">
+    <link href="{{asset('templates/fiche/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
 
     <!-- Fonts -->
@@ -15,15 +15,15 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="{{asset('fiche/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('fiche/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
-    <link href="{{asset('fiche/assets/vendor/aos/aos.css')}}" rel="stylesheet">
-    <link href="{{asset('fiche/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
-    <link href="{{asset('fiche/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+    <link href="{{asset('templates/fiche/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('templates/fiche/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('templates/fiche/assets/vendor/aos/aos.css')}}" rel="stylesheet">
+    <link href="{{asset('templates/fiche/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+    <link href="{{asset('templates/fiche/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
     <script src="https://kit.fontawesome.com/cdbf73e12c.js" crossorigin="anonymous"></script>
 
     <!-- Main CSS File -->
-    <link href="{{asset('fiche/assets/css/main.css')}}" rel="stylesheet">
+    <link href="{{asset('templates/fiche/assets/css/main.css')}}" rel="stylesheet">
 
     <!-- =======================================================
   * Template Name: Dewi
