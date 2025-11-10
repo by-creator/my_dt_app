3cc4eb023431434c656e181feaccddf6d2480d17
by-creator <marcdamien04@gmail.com>
2025-11-10 13:54:30 +0000

    update

--- resources/views/emails/rattachement_bl_invalide.blade.php
+++ resources/views/emails/rattachement_bl_invalide.blade.php
@@ -18,7 +18,7 @@ Votre dossier dont voici le numéro de BL **{{ $bl }}** a été rejeté pour le 
 
 Merci de refaire votre demande en complétant le dossier si nécessaire.
 
-Pour accéder à notre plateforme en cliquant sur le lien ci-dessous :
+Pour accéder à notre plateforme merci de cliquer sur le lien ci-dessous :
 
 @component('mail::button', ['url' => 'https://site-dt-production-98050a853413.herokuapp.com/demat'])
 LIEN D'ACCÈS PLATEFORME
