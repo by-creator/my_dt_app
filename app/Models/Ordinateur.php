7bd7b1ab57d10e2c88e2e3e21130975801fbd846
by-creator <marcdamien04@gmail.com>
2025-11-12 21:08:16 +0000

    update

--- app/Models/Ordinateur.php
+++ app/Models/Ordinateur.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Traits\ConvertsDates;
+
 
 class Ordinateur extends Model
 {
-    use HasFactory;
+    use HasFactory, ConvertsDates;
     
      protected $fillable = [
         'serie',
@@ -16,5 +18,19 @@ class Ordinateur extends Model
         'utilisateur',
         'service',
         'site',
+        'date_reception',
+        'date_deploiement'
     ];
+
+    protected $casts = ['date_reception' => 'datetime', 'date_deploiement' => 'datetime'];
+
+    public function getDateReceptionFormattedAttribute()
+    {
+        return $this->date_reception ? $this->date_reception->format('d/m/Y H:i') : null;
+    }
+
+    public function getDateDeploiementFormattedAttribute()
+    {
+        return $this->date_deploiement ? $this->date_deploiement->format('d/m/Y H:i') : null;
+    }
 }
