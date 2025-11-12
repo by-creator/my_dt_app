34df2b3f8d0bceacb5702c36a54dd8bd93b56f25
by-creator <marcdamien04@gmail.com>
2025-11-12 22:44:57 +0000

    update

--- resources/views/partials/stock/menu_stock.blade.php
+++ resources/views/partials/stock/menu_stock.blade.php
@@ -76,9 +76,25 @@
         <span>Stock</span>
     </a>
     <ul class="submenu">
-        <li class="submenu-item">
+                <li class="submenu-item">
 
             <a href="{{ route('ordinateur.index') }}"><i class="fa-solid fa-computer"></i> Ordinateur</a>
+        </li>
+        <li class="submenu-item">
+
+            <a href="{{ route('clavier.index') }}"><i class="fa-solid fa-keyboard"></i> Clavier</a>
+        </li>
+        <li class="submenu-item">
+
+            <a href="{{ route('souris.index') }}"><i class="fa-solid fa-computer-mouse"></i> Souris</a>
+        </li>
+        <li class="submenu-item">
+
+            <a href="{{ route('ecran.index') }}"><i class="fa-solid fa-desktop"></i> Ecran</a>
+        </li>
+        <li class="submenu-item">
+
+            <a href="{{ route('station.index') }}"><i class="fa-solid fa-shuffle"></i> Station</a>
         </li>
         <li class="submenu-item ">
             <a href="{{ route('telephone-fixe.index') }}"><i class="fa-solid fa-phone"></i> Postes fixes</a>
