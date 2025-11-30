b62c652c68a0b2b233f68f5090c1a90fecdc14ff
by-creator <marcdamien04@gmail.com>
2025-11-30 11:16:36 +0000

    update

--- app/Http/Controllers/DossierFacturationController.php
+++ app/Http/Controllers/DossierFacturationController.php
@@ -7,9 +7,6 @@ use App\Models\DossierFacturation;
 use App\Models\RattachementBl;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Log;
-use Carbon\Carbon;
 
 
 class DossierFacturationController extends Controller
@@ -68,64 +65,8 @@ class DossierFacturationController extends Controller
         return back()->with('success', 'Documents enregistrés avec succès !');
     }
 
-    public function proforma()
-    {
-        $dossiers = DossierFacturation::orderBy('id', 'desc')->get();
-        $users = User::all();
-        return view('dossier_facturation.proforma', compact('dossiers', 'users'));
-    }
-
-    public function proformaGenerate(Request $request, $id)
-    {
-        $request->validate([
-            'documentDate' => 'required|date',
-        ]);
-
-        $dossier = DossierFacturation::findOrFail($id);
-
-        // Exemple : générer le document avec la date choisie
-        $date = Carbon::parse($request->input('documentDate'));
-
-
-        // On récupère le rattachement BL
-        $rattachement = $dossier->rattachement_bl;
-
-        // On prépare les données à envoyer dans le mail
-        $data = [
-            'date' => $date,
-            'prenom'  => $rattachement->prenom,
-            'nom'     => $rattachement->nom,
-            'email'   => $rattachement->email,
-            'bl'      => $rattachement->bl,
-            'compte'  => $rattachement->compte,
-        ];
-
-        // Liste des destinataires
-        $destinataires = [
-            'noreplysitedt@gmail.com'
-        ];
-
-        // Envoi du mail
-        Mail::to($destinataires)->send(new ProformaGenerateMail($data));
-
-
-        Log::info('Demande de facture pro-forma envoyée');
-
-
-
-        $dossier->date_proforma = $date;
-
-        $dossier->statut = "EN ATTENTE PROFORMA";
-
-        // Ici tu peux mettre à jour updated_at si nécessaire
-        $dossier->updated_at = now(); // ou la date spécifique
-        
-
-        // Sauvegarde les changements
-        $dossier->save();
-
-        return redirect()->back()->with('success', "Votre facture sera disponible dans 10 minutes ");
-    }
+    
+    
 
 
     public function facture()
