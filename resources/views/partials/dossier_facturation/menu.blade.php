962b9567eb0d6fdecb175c461b98c6c088d7bcb6
by-creator <marcdamien04@gmail.com>
2025-12-04 19:20:55 +0000

    update

--- resources/views/partials/dossier_facturation/menu.blade.php
+++ resources/views/partials/dossier_facturation/menu.blade.php
@@ -1,33 +1,33 @@
 
 <li class="sidebar-item">
-    <a href="{{ route('dossier_facturation.validation-index') }}" class='sidebar-link'>
+    <a class='sidebar-link' href="{{ route('dossier_facturation.validation-index') }}" class='sidebar-link'>
         <i class="fa-solid fa-check-to-slot"></i>
         <span>Validation</span>
     </a>
 </li>
 <li class="sidebar-item">
-    <a href="{{ route('dossier_facturation.paiement-index') }}" class='sidebar-link'>
+    <a class='sidebar-link' href="{{ route('dossier_facturation.paiement-index') }}" class='sidebar-link'>
         <i class="fa-solid fa-money-bill"></i>
         <span>Paiement</span>
     </a>
 </li>
 <li class="sidebar-item">
-    <a href="#" class='sidebar-link'>
+    <a class='sidebar-link' href="#" class='sidebar-link'>
         <i class="fa-solid fa-clipboard-list"></i>
         <span>Demande de réduction</span>
     </a>
 </li>
 <li class="sidebar-item  has-sub ">
-    <a href="#" class='sidebar-link'>
+    <a class='sidebar-link' href="#" class='sidebar-link'>
         <i class="fa-solid fa-circle-info"></i>
         <span>Comment ça marche </span>
     </a>
     <ul class="submenu">
         <li class="submenu-item">
-            <a href="{{ route('dossier_facturation.tuto-pdf-index') }}"><i class="fa-solid fa-clipboard-list"></i> Format PDF</a>
+            <a class='sidebar-link' href="{{ route('dossier_facturation.tuto-pdf-index') }}"><i class="fa-solid fa-clipboard-list"></i> Format PDF</a>
         </li>
         <li class="submenu-item ">
-            <a href="{{ route('dossier_facturation.tuto-video-index') }}"><i class="fa-solid fa-video"></i> Format VIDEO</a>
+            <a class='sidebar-link' href="{{ route('dossier_facturation.tuto-video-index') }}"><i class="fa-solid fa-video"></i> Format VIDEO</a>
         </li>
     </ul>
 </li>
