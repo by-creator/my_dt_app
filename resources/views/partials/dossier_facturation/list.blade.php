9e69c24fab60e629f094198e3818a8982ca3086d
by-creator <marcdamien04@gmail.com>
2025-12-10 23:25:58 +0000

    update_facturation

--- resources/views/partials/dossier_facturation/list.blade.php
+++ resources/views/partials/dossier_facturation/list.blade.php
@@ -33,7 +33,7 @@
                     name="search"
                     class="form-control shadow-sm"
                     style="height: 38px;"
-                    placeholder="Entrez le BL…"
+                    placeholder="Entrez le BL"
                     value="{{ request('search') }}">
 
                 <!-- Bouton -->
