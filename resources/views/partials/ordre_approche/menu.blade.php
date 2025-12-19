fe7f5262c0b5b63cb10e48aa3fed5d643c50ec79
Marc Bongoyeba (AGL) <marc.bongoyeba@dakar-terminal.com>
2025-12-19 16:54:47 +0000

    update

--- resources/views/partials/ordre_approche/menu.blade.php
+++ resources/views/partials/ordre_approche/menu.blade.php
@@ -1,16 +1,6 @@
-<li class="sidebar-item  has-sub active">
-    <a href="#" class='sidebar-link'>
+<li class="sidebar-item">
+    <a href="{{route('ordre_approche.index')}}" class='sidebar-link'>
         <i class="fa-solid fa-rectangle-list"></i>
         <span>Ordre Approche</span>
     </a>
-    <ul class="submenu">
-        <li class="submenu-item">
-
-            <a href="#"><i class="fa-solid fa-clipboard"></i> Gestion des ordres</a>
-        </li>
-        <li class="submenu-item">
-
-            <a href="#"><i class="fa-solid fa-clipboard"></i> Liste des ordres</a>
-        </li>
-    </ul>
 </li>
