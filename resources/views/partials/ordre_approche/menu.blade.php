d9f5a643c56cd2de1708e18abb1ffca19a63b0c3
Marc Bongoyeba (AGL) <marc.bongoyeba@dakar-terminal.com>
2025-12-19 16:55:09 +0000

    update

--- resources/views/partials/ordre_approche/menu.blade.php
+++ resources/views/partials/ordre_approche/menu.blade.php
@@ -1,4 +1,4 @@
-<li class="sidebar-item">
+<li class="sidebar-item active">
     <a href="{{route('ordre_approche.index')}}" class='sidebar-link'>
         <i class="fa-solid fa-rectangle-list"></i>
         <span>Ordre Approche</span>
