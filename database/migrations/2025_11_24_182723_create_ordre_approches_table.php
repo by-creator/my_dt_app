19d751bfb4728001fa065e433aaab6bfc11c441b
Marc Bongoyeba (AGL) <marc.bongoyeba@dakar-terminal.com>
2025-12-24 13:34:36 +0000

    update

--- database/migrations/2025_11_24_182723_create_ordre_approches_table.php
+++ database/migrations/2025_11_24_182723_create_ordre_approches_table.php
@@ -13,25 +13,35 @@ return new class extends Migration
     {
         Schema::create('ordre_approches', function (Blueprint $table) {
             $table->id();
-            $table->dateTime('date')->nullable();
-            $table->dateTime('time')->nullable();
-            $table->string('zone')->nullable();
-            $table->string('chassis')->nullable();
-            $table->string('poids')->nullable();
-            $table->string('bae')->nullable();
-            $table->string('booking')->nullable();
-            $table->string('vessel')->nullable();
-            $table->string('call_number')->nullable();
-            $table->string('vessel_arrival_date')->nullable();
-            $table->string('shipping_line')->nullable();
-            $table->string('category')->nullable();
-            $table->string('type')->nullable();
-            $table->string('model')->nullable();
-            $table->string('client')->nullable();
-            $table->string('chauffeur')->nullable();
-            $table->string('permis')->nullable();
-            $table->string('pointeur')->nullable();
-            $table->string('responsable')->nullable();
+            $table->string('Terminal')->nullable();
+            $table->string('Shipowner')->nullable();
+            $table->string('ItemNumber')->nullable();
+            $table->string('Item_Type')->nullable();
+            $table->string('Item_Code')->nullable();
+            $table->string('BlNumber')->nullable();
+            $table->string('FinalDestinationCountry')->nullable();
+            $table->string('Description_')->nullable();
+            $table->string('TEU')->nullable();
+            $table->string('Volume')->nullable();
+            $table->string('Weight_')->nullable();
+            $table->string('YardZoneType')->nullable();
+            $table->string('Zone')->nullable();
+            $table->string('Type_Veh')->nullable();
+            $table->string('TypeDeMarchandise')->nullable();
+            $table->string('POD')->nullable();
+            $table->string('YardZone')->nullable();
+            $table->string('consignee')->nullable();
+            $table->string('callNumber')->nullable();
+            $table->string('Vessel')->nullable();
+            $table->string('ETA')->nullable();
+            $table->string('vesselarrivaldate')->nullable();
+            $table->string('Cycle')->nullable();
+            $table->string('Yard Quantity')->nullable();
+            $table->string('DAYS SINCE IN')->nullable();
+            $table->string('Dwelltime')->nullable();
+
+            $table->datetime('date');
+            $table->datetime('time');
             $table->timestamps();
         });
     }
