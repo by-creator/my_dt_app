1f7f7f737b72e75aed418e5f9d089d72de947463
BONGO-YEBA Marc <45905771+by-creator@users.noreply.github.com>
2026-03-12 09:33:11 +0000

    Update role checks for sidebar and welcome message

--- resources/views/ies/reset-password.blade.php
+++ resources/views/ies/reset-password.blade.php
@@ -4,7 +4,7 @@
 
 @section('sidebar-menu')
     @auth
-       @if(Auth::user()->role->name == "ADMIN" || Auth::user()->role->name == "SUPER_U")
+       @if(Auth::user()->role->name == "ADMIN" || Auth::user()->role->name == "SUPER_U" || Auth::user()->role->name == "FACTURATION")
                         @include('partials.facturation.menu_facturation')
                         @else
                         @endif
@@ -24,7 +24,7 @@
             <h3>Bienvenu(e) {{ $user->name }} </h3>
         </div>
           <div class="page-content">
-                @if(Auth::user()->role->name == "ADMIN" || Auth::user()->role->name == "SUPER_U")
+                @if(Auth::user()->role->name == "ADMIN" || Auth::user()->role->name == "SUPER_U" || Auth::user()->role->name == "FACTURATION")
                 @if (session('reset'))
                 <script>
                     Swal.fire({
